9b254238548c80e38b5fb177899d55092641574e
shivanand0 <73774388+shivanand0@users.noreply.github.com>
2021-04-04 19:06:21 +0530

    Add files via upload

---
+++ guessNumGame.java
@@ -0,0 +1,63 @@
+/*
+    Create a class Game, which allows a user to play "Guess the Number"
+    game once. Game should have the following methods:
+    1. Constructor to generate the random number
+    2. takeUserInput() to take a user input of number
+    3. isCorrectNumber() to detect whether the number entered by the user is true
+    4. getter and setter for noOfGuesses
+    Use properties such as noOfGuesses(int), etc to get this task done!
+*/
+import java.util.Random;
+import java.util.Scanner;
+
+class Game{
+    public int num; //number to guess
+    public int inputNum; //user inputed number
+    public int noOfGuesses = 0;
+
+    // public int getNoOfGuesses() {
+    //     return noOfGuesses;
+    // }
+
+    // public void setNoOfGuesses(int noOfGuesses) {
+    //     this.noOfGuesses = noOfGuesses;
+    // }
+
+    Game(){ //constructor to generate random number upto 100
+        Random rand = new Random();
+        this.num = rand.nextInt(100);
+        // System.out.println("NUMBER: "+num);
+    }
+    void takeUserInput(){
+        System.out.print("Guess the number: ");
+        Scanner sc = new Scanner(System.in);
+        inputNum = sc.nextInt();
+    }
+    boolean isCorrectNumber(){
+        noOfGuesses++;
+        if (inputNum==num){
+            System.out.format("Yes you guessed it right, it was %d\nYou guessed it in %d attempts", num, noOfGuesses);
+            return true;
+        }
+        else if(inputNum<num){
+            System.out.println("Too low...");
+        }
+        else if(inputNum>num){
+            System.out.println("Too high...");
+        }
+        return false;
+    }
+
+}
+public class guessNumGame {
+    public static void main(String[] args) {
+        Game g = new Game(); //Game object
+        boolean res= false;
+
+        while(!res){ //repeat until user inputs correct number
+        g.takeUserInput();
+        res = g.isCorrectNumber();
+        }
+
+    }
+}
