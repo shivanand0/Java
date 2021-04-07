ecea907ebda044822d16b1179fae0580ffe16150
shivanand0 <73774388+shivanand0@users.noreply.github.com>
2021-04-07 18:00:20 +0530

    Update varargs.java

--- varargs.java
+++ varargs.java
@@ -1,7 +1,11 @@
+/*
+--> The varrags allows the method to accept zero or muliple arguments. Before varargs either we use overloaded method or take an array as the method parameter.
+--> If we don't know how many argument we will have to pass in the method, varargs is the better approach, we don't have to provide overloaded methods.
+*/
 public class varargs {
     
-    public static void add(int ...arr){ //parameters passed as an array
-        System.out.println("Number of arguments: " + arr.length); 
+        static void add(int ...arr){ //parameters passed as an array
+        System.out.println("Number of parameters passed: " + arr.length); 
         int sum = 0;
 
         for(int i: arr){
@@ -13,8 +17,9 @@ public class varargs {
     public static void main(String[] args) {
         // Calling the varargs method with different number 
         // of parameters 
-        add(100,100,100,50,50);         // one parameter 
-        add(1, 2, 3);  // four parameters 
-        add(2,2);            // no parameter 
+        add(100,100,100,50,50);         // five parameter 
+        add(1, 2, 3);                   // three parameters 
+        add(2,2);                       // two parameters
+        add();                          //zero parameters
     }
 }
